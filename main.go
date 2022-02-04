fbfc834dd3499cd01f359aada7f7f7bf47800c22
astevenaa <astevenaa@gmail.com>
2022-02-04 11:32:16 -0700

    all routes addded

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+import(
+	"github.com/AcevedoEsteban/goEcommerce-yt/controllers"
+	"github.com/AcevedoEsteban/goEcommerce-yt/database"
+	"github.com/AcevedoEsteban/goEcommerce-yt/middleware"
+	"github.com/AcevedoEsteban/goEcommerce-yt/controllers"
+	"github.com/AcevedoEsteban/goEcommerce-yt/routes"
+	"github.com/gin-gonic/gin"
+
+) 
+
+func main(){
+	port := os.Getenv("PORT")
+	if port == ""{
+		port ="8000"
+	}
+
+	app := controllers.NewApplication(database.Client, "Products"), database.UserData(database.Client, "Users")
+	
+	router := gin.New()
+	router.Use(gin.Logger())
+	
+	routes.UserRouters(routes)
+	router.Use(middleware.Authentication())
+	
+	router.GET("/addtocart", app.AddToCart())
+	router.GET("/removeitem", app.RemoveItem())
+	router.GET("/cartcheckout", app.BuyFromCart())
+	router.GET("/instantbuy", app.InstantBuy())
+
+	log.Fatal(router.Run(":" + port))
+
+
+}
